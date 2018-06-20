fb733b46980884398a4dc50e74cb8f107208408d
Ainesh <ainesh3098@gmail.com>
2018-06-20 12:19:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ainesh/navigation/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.ainesh.navigation;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+  //  TextView tx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+         /*tx = (TextView)findViewById(R.id.signinView);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Ubuntu-Bold.ttf");
+
+        tx.setTypeface(custom_font);*/
+
+   }
+}
